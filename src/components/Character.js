f73aeae31d84b4a7c0a6be36e17d4e7b5a4d355d
John Chamberlin <johnchamberlin00@gmail.com>
2020-11-06 09:29:18 -0800

    mvp done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+
+export default function Character(props) {
+    const { name, birthyear } = props;
+
+
+
+
+
+
+
+
+    return (
+        <StyledImage className = 'character-container'>
+            <h2>{name} was born in {birthyear}</h2>
+        </StyledImage>
+
+
+
+
+
+
+    )
+
+}
+
+
+
+const StyledImage = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    border-color: ${(pr) => pr.theme.primaryColor};
+    width:50%;
+    margin:1rem;
+    color: ${(pr) => pr.theme.tertiaryColor}
+
+
+
+`
---
+++ src/theme/theme.js
@@ -0,0 +1,11 @@
+export default {
+    primaryColor: '#FFE81F',
+    secondaryColor: 'crimson',
+    tertiaryColor: '#15f2fd',
+    danger: 'red',
+    black: 'black',
+    white: 'white',
+
+
+
+}
